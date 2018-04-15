ba63d8e4103ece5781fd1a9109c24d59144512c7
n0807701j2d2 <n0807701j2d2@gmail.com>
2018-04-15 18:03:09 -0500

    Fixes merge

--- controllers/StudentsController.php
+++ controllers/StudentsController.php
@@ -2,7 +2,6 @@
 
 class StudentsController extends Controller
 {
-<<<<<<< HEAD
 	public function display()
 	{
 		if(Session::isLoggedIn())
@@ -22,14 +21,4 @@ class StudentsController extends Controller
 	{
 
 	}
-=======
-  public function display()
-  {
-    (new EnterInfoView())->render();
-  }
-  public function done()
-  {
-    (new DoneView())->render();
-  }
->>>>>>> a85c0baafa3adcb837b343bf416ff8bb805f5c1b
 }
