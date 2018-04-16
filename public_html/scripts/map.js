c975e56cd5d28f83f5833d1db848191c98b86868
CRHSCS273 <CRHSCS273@KATYISD.ORG>
2018-04-16 09:36:28 -0500

    almost made the add a row to map function for the DB

--- public_html/scripts/map.js
+++ public_html/scripts/map.js
@@ -7,9 +7,10 @@ $(document).ready(function() {
     $(this).children().css("display","none");
   })
 
-
+  $("#addrowbutton").click(function(){addRow();});
 
 });
+
   function gotolot(lot)
   {
     location.replace("/?p=map&do="+lot);
@@ -17,4 +18,28 @@ $(document).ready(function() {
   function gotohub()
   {
     location.replace("/?p=map");
+  }
+//just a function to add rows in the database, alter it to what ever you want
+  function addRow()
+  {
+    console.log("i am adding a row now!");
+    var values = {
+      'addrow' : true,
+      'x' : 38.3,
+      'y' : 3.8,
+      'width' : 2.4,
+      'height' : 5.5,
+      'spot_num' : 1000,
+      'num_of_spots' : 13,
+      'spacing' : 2
+    };
+    request = $.ajax({
+      type: "post",
+      url: "/?p=map&do=addRow",
+      data: values
+    });
+    request.done(function (){
+      //window.location.replace("/?p=map&do=paclot");
+      console.log("ya i just finished")
+    });
   }
